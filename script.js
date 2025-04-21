3bff5980c0c8f0d0e22562bc1d3adddf278dde25
YusufXojimurodov <xojimurodovyusufjon2@gmail.com>
2025-04-21 18:47:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let ism = prompt("Ismingizni kiriting")
+
+let h1 = document.createElement('h1')
+
+h1.innerHTML = ism
+
+document.body.append(h1)
+
+document.body.style.backgroundColor = 'blue'
+
+document.body.style.color = 'yellow'
+
+document.body.style.fontSize = '48px'
+
+document.body.style.textAlign = 'center'
